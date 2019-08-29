4b64340b6cac802acbee770cca11eb4014ce43f7
Etheon <etheanore@gmail.com>
2019-08-29 14:44:19 +0200

    added event listener (empty right now)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import {pageCreator} from './pageLoader'
 
+function hello() {
+    console.log("yo")
+}
+
+const about = document.getElementById('About')
+console.log(about)
+about.addEventListener('click', hello);
+
+
+
 pageCreator;
